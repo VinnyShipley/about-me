d133701baaf29ce2ee908903b208a00d381a0d0c
Vinny <vinnyshipley@gmail.com>
2022-04-06 19:48:50 -0700

    built q6+7, scoring

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let score = 0;
+
 //welcome message
 alert('Welcome to my webpage! Consider this my greeting to you! I hope you enjoy your visit!');
 
@@ -13,6 +15,7 @@ let questionOne = prompt('Have I ever tended bar before?').toLowerCase();
 
 if(questionOne === 'yes' || questionOne === 'y'){
   //console.log('q 1 correct');
+  score = score + 1;
   alert(`Correct ${siteVisitor}, I have in fact tended a couple bars in my day. Maybe I even served you before!`);
 } else if(questionOne === 'no' || questionOne === 'n'){
   alert(`Sorry ${siteVisitor}, but I've spent many a year behind the stick at a couple places in the Bellingham area.`);
@@ -23,6 +26,7 @@ let questionTwo = prompt('Have I ever gone skydiving before?').toLowerCase();
 if(questionTwo === 'yes' || questionTwo ==='y'){
   //console.log('question 2 correct');
   alert(`That is also correct ${siteVisitor}, I have gone skydiving before. I only had three panic attacks on the flight up`);
+  score = score + 1;
 } else if(questionTwo === 'no' || questionTwo === 'n'){
   alert(`Sorry ${siteVisitor}, but I have jumped out of a perfectly good airplane.`);
 } else alert(`Hold your horses there ${siteVisitor}. I thought I was pretty clear I needeed a yes, no, y, or n answer from you. This game is a lot more fun if you do`);
@@ -34,6 +38,7 @@ if(questionThree === 'yes' || questionThree ==='y'){
   alert(`Nope. Sorry to disappoint you ${siteVisitor}, but this site isn't me just bragging, it's also about asking the hard hitting questions like this next one.`);
 } else if(questionThree === 'no' || questionThree === 'n'){
   alert(`You're right ${siteVisitor}! I wouldn't try them either way, but I feel like elephant taste terrible for some reason`);
+  score = score + 1;
 } else alert(`Hold your horses there ${siteVisitor}. I thought I was pretty clear I needeed a yes, no, y, or n answer from you. This game is a lot more fun if you do`);
 
 let questionFour = prompt('Did the act of googling if it was legal to eat elephant make me sad while writing this codeblock?').toLowerCase();
@@ -41,6 +46,7 @@ let questionFour = prompt('Did the act of googling if it was legal to eat elepha
 if(questionFour === 'yes' || questionFour ==='y'){
   //console.log('q 4 correct');
   alert(`Good call, ${siteVisitor}. But apparently elephant tastes like veal. Troubling stuff.`);
+  score = score + 1;
 } else if(questionFour === 'no' || questionFour === 'n'){
   alert(`Do you think I'm heartless, ${siteVisitor}? Shame on you.`);
 } else alert(`Hold your horses there ${siteVisitor}. I thought I was pretty clear I needeed a yes, no, y, or n answer from you. This game is a lot more fun if you do`);
@@ -50,6 +56,60 @@ let questionFive = prompt('Should I lay off the elephant material?').toLowerCase
 if(questionFive === 'yes' || questionFive ==='y'){
   //console.log('q 5 correct');
   alert('Agreed');
+  score = score + 1;
 } else if(questionFive === 'no' || questionFive === 'n'){
   alert('Have I not done enough?');
 } else alert(`Hold your horses there ${siteVisitor}. I thought I was pretty clear I needeed a yes, no, y, or n answer from you. This game is a lot more fun if you do`);
+
+
+//6th question with 4 max guesses
+
+//Katharine, Guy, Elizabeth all helped out with the 6th question, and I used a lot of the same code from q6 in q 7
+
+for(let i = 0; i <= 4; i++){
+
+  let questionSix = prompt('What is the magic number?');
+  let south = 3;
+
+  if((questionSix > south) && (i <= 2)){
+    alert('Too high');
+  } else if((questionSix < south) && (i <= 2)){
+    alert('Too low');
+  } else if((questionSix = south) && (i <= 2)){
+    alert('You got it!');
+    score = score + 1;
+    break;
+  } else {
+    alert('Better luck next time. It was three');
+    break;
+  }
+}
+
+let myArray = ['daiquiri', 'old fashioned', 'manhattan'];
+
+for(let i = 0; i <= 5; i++){
+
+
+  let questionSeven = prompt('What is one of my three favorite cockatails?').toLowerCase();
+  if(questionSeven === myArray[0]){
+    alert('Daiquiris are one of my favorites!');
+    score = score +1;
+    break;
+  } else if(questionSeven === myArray[1]){
+    alert('Old fashioneds are one of my favorites!');
+    score = score +1;
+    break;
+  } else if(questionSeven === myArray[2]){
+    alert('Manhattans are one of my favorites!');
+    score = score +1;
+    break;
+  } else if((questionSeven !== myArray) && (i <= 5)){
+    alert(`That's not one of my favorite drinks.`);
+  } else{
+    alert('None of those were my favorites. Better luck next time.');
+  }
+}
+
+alert(`The list of my favorite drinks are ${myArray}`);
+
+alert(`Congratulations ${siteVisitor}! You got ${score} of 7 questions correct!`);
